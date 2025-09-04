db2b28b44146de2bbe45975934d24cc3513694b7
Nirav1630 <155763319+Nirav1630@users.noreply.github.com>
2025-09-04 11:56:07 +0530

    Add files via upload

---
+++ github.py
@@ -0,0 +1,11 @@
+echo "# pro-data" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Nirav1630/pro-data.git
+git push -u origin main
+
+git remote add origin https://github.com/Nirav1630/pro-data.git
+git branch -M main
+git push -u origin main
